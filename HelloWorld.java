848735003bec4be43df2a771b33f0bea75d40f56
David Schilmoeller <dave.schilmoeller@gmail.com>
2023-12-13 12:42:42 -0600

    codecademy projects and notes.

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+class HelloYou {
+  public static void main(String[] args) {
+    System.out.println("Hello World");
+    int year = 2001;
+    // Note Double vs Single quotes. Caps on String vs everything else.
+    String title = "Shrek";
+    char genre = 'C';
+    double runtime = 1.58;
+    boolean isPG = true;
+
+
+  }
+}
